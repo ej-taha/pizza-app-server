960694b795589b2c011ae76d06da4e13e1ba67d9
ej_taha <t.e.j@hotmail.fr>
2020-07-27 17:56:23 +0100

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 require('./models/topping');
 require('./models/pizza');
 require('./config/database');
+require('./services/service');
 import server from './config/server';
 
 const PORT = process.env.PORT || 8081;
